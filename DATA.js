31fb1038e868cb7f7a1ded55841c7f455788a54a
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-02 19:42:42 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,73 @@
+export const item = {
+    "id": "aa8db837-f798-4266-a7de-e798dda49ca9",
+    "app_name": "community",
+    "story_list": [
+        {
+            "id": "3b69d9c9-f2eb-4a61-a736-5462ea8cbda6",
+            "story_image_list": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/142.png",
+            "user_name": "group"
+        },
+        {
+            "id": "6e956e93-f4e1-43fd-a47f-9edbc1fde307",
+            "story_image_list": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png",
+            "user_name": "energy"
+        },
+        {
+            "id": "a31ab63f-24c2-4005-ab0a-41ca2be3ddb4",
+            "story_image_list": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/236.png",
+            "user_name": "feeling"
+        },
+        {
+            "id": "2a16d1a2-b5d5-4560-8c59-ba4e8814536e",
+            "story_image_list": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/30.png",
+            "user_name": "threat"
+        },
+        {
+            "id": "d67a7507-4d96-4ba4-8da6-8538ebb30992",
+            "story_image_list": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/294.png",
+            "user_name": "parent"
+        }
+    ],
+    "feed_list": [
+        {
+            "id": "8252d077-b41f-463b-bd71-1241628c7e59",
+            "post_user": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/45.png",
+            "news_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/230.png",
+            "num_like": "evidence",
+            "description": "Kind long wife do might role."
+        },
+        {
+            "id": "a8014bcd-a597-40c4-984e-4c4e7ce11264",
+            "post_user": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/297.png",
+            "news_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/69.png",
+            "num_like": "rest",
+            "description": "My five forget ask likely about. Wide its its long between number. Surface own age general."
+        },
+        {
+            "id": "b287860d-66b1-4c70-8e8b-5d51ebb4c5b8",
+            "post_user": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/205.png",
+            "news_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/196.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/88.png",
+            "num_like": "individual",
+            "description": "Force part land green industry position. Eye fact end outside nor beautiful. Child prove shoulder here ever win expect."
+        },
+        {
+            "id": "44e34a02-3c7b-4b50-b176-75b9dff56479",
+            "post_user": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/217.png",
+            "news_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/227.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/160.png",
+            "num_like": "happy",
+            "description": "School with model pattern into challenge relate. General magazine positive yet store establish easy. Few finish church."
+        },
+        {
+            "id": "ee211d15-6b6a-4b43-a5d4-91a52388fe3b",
+            "post_user": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/203.png",
+            "news_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/287.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/300.png",
+            "num_like": "shake",
+            "description": "Truth effort collection away push road."
+        }
+    ]
+}
